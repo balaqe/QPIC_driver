c2efbb8bb9481b3757ec86f23d059e9ec3afa272
Balage <basza22@student.sdu.dk>
2026-04-29 11:27:27 +0200

    Angles found but mesh structure is faulty

--- decomp/tests/arbitrary_q.py
+++ decomp/tests/arbitrary_q.py
@@ -1,27 +1,31 @@
 import numpy as np
-import sympy as sp
-import deepcopy
+# import sympy as sp
+from copy import deepcopy
 
 class Mzi:
     def __init__(self):
-        self.layer_num = None # Index of the accompanying layer
-        self.index = None # Index within a given layer
-        self.upper_pred = None # Reference to the upper predecessor
-        self.lower_pred = None # Lower predecessor
-        self.upper_succ = None # Upper successor
-        self.lower_succ = None # Lower successor
+        self.active = False # Switch for indicating whether the mzi is active in the grid
 
-        self.sigma = None
-        self.delta = None
+        self.layer_num: int # Index of the accompanying layer
+        self.index: int # Index within a given layer
+        self.upper_pred: Mzi | Phase_shifter # Reference to the upper predecessor
+        self.lower_pred: Mzi | Phase_shifter # Lower predecessor
+        self.upper_succ: Mzi | Phase_shifter # Upper successor
+        self.lower_succ: Mzi | Phase_shifter # Lower successor
+
+        self.sigma: complex
+        self.delta: complex
         self.phi = [complex, complex] # Phi1 and phi2
-        self.phase_diff = 0 # Phase difference to be added to lower_pred
+        self.phase_diff: complex # Phase difference to be added to lower_pred
 
 class Phase_shifter:
     def __init__(self):
-        self.layer_num = None # Index of the accompanying layer
-        self.index = None # Index within a given layer
-        self.pred = None # Reference to the predecessor
-        self.succ = None # Successor
+        self.active = False
+
+        self.layer_num: int # Index of the accompanying layer
+        self.index: int # Index within a given layer
+        self.pred: Mzi | Phase_shifter # Reference to the predecessor
+        self.succ: Mzi | Phase_shifter # Successor
 
         self.phi = None
 
@@ -42,31 +46,40 @@ class Compiler:
         y_shape = U.shape[0]
 
         # Initialize MZI structure given the mesh size
-        self.mzi = [[None for _ in range(x_shape+1)] for _ in range(y_shape)] # x_shape + 1 to account for initial phase shifters
-        self.phase_shifter = [[None for _ in range(x_shape+1)] for _ in range(y_shape)]
+        # self.mzi = [[Mzi() for _ in range(x_shape+1)] for _ in range(y_shape)] # x_shape + 1 to account for initial phase shifters
+        self.mzi = [[Mzi() for _ in range(x_shape)] for _ in range(y_shape+1)] # y_shape + 1 to account for initial phase shifters
+        # self.phase_shifter = [[Phase_shifter() for _ in range(x_shape+1)] for _ in range(y_shape)]
+        self.phase_shifter = [[Phase_shifter() for _ in range(x_shape)] for _ in range(y_shape+1)]
 
         V = deepcopy(U)
 
         for diag_id in range(x_shape): # Diagonals starting from the bottom left
             for elem_id in range(diag_id + 1):
+            # for elem_id in range(diag_id):
                 y = y_shape - diag_id + elem_id - 1
                 x = elem_id
-                print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({x}, {y})")
+                # print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({x}, {y})")
 
                 if diag_id % 2 == 0: # j = 1, 3, 5, ... (j starts at 1 while diag_id starts at 0)
                     # VM
-                    delta = np.atan(V[x][y] / V[x+1][y])
-                    sigma = sympy.angle(V[x][y])
-                    phase_diff = V[x][y] - V[x_shape-1 - x][y_shape-1 - y]
+                    delta = 0
+                    if V[y][x+1] != 0:
+                        delta = np.atan(-V[y][x] / V[y][x+1])
+                    else:
+                        delta = np.pi # Pi shift which produces identity
+
+                    sigma = np.angle(V[y][x])
+                    phase_diff = np.angle(V[y][x]) - np.angle(V[x][y])
+                    # phase_diff = np.angle(V[y][x]) - np.angle(V[y_shape-1 - y][x_shape-1 - x])
 
                     m = np.array([[np.exp(sigma*1j) * np.sin(delta), np.exp(sigma*1j) * np.cos(delta)],\
                                 [np.exp(sigma*1j) * np.cos(delta), -np.exp(sigma*1j) * np.sin(delta)]])
 
-                    x_offset = x
+                    x_offset = x if x < x_shape-2 else x_shape-2
                     y_offset = x_offset
 
-                    M = np.identity(x_shape)
-                    M[x_offset:x_offset+m.shape[0], y_offset:y_offset+m.shape[1]] = m
+                    M = np.identity(x_shape, dtype="complex")
+                    M[y_offset:y_offset+m.shape[0], x_offset:x_offset+m.shape[1]] = m
 
                     V = V @ M
 
@@ -75,57 +88,79 @@ class Compiler:
                     mzi.index = diag_id # Don't divide by 2 to allow half-step offset between layers
                     mzi.delta = delta
                     mzi.sigma = sigma
-                    phase_diff = phase_diff
+                    mzi.phase_diff = phase_diff
+                    mzi.active = True
                     self.insert_mzi(mzi)
 
-                else: # j = 0, 2, 4, 
+                else: # j = 0, 2, 4, ...
                     # MV
+                    delta = 0
+                    if V[y][x] != 0:
+                        delta = np.atan(-V[y-1][x] / V[y][x])
+                    else:
+                        delta = np.pi
+                    sigma = np.angle(V[y][x])
+                    phase_diff = np.angle(V[y][x]) - np.angle(V[x][y])
+
+                    m = np.array([[np.exp(sigma*1j) * np.sin(delta), np.exp(sigma*1j) * np.cos(delta)],\
+                                [np.exp(sigma*1j) * np.cos(delta), -np.exp(sigma*1j) * np.sin(delta)]])
+
+                    x_offset = x if x < x_shape-2 else x_shape-2
+                    y_offset = x_offset
+
+                    M = np.identity(x_shape, dtype="complex")
+                    M[y_offset:y_offset+m.shape[0], x_offset:x_offset+m.shape[1]] = m
+
+                    V = M @ V
 
                     mzi = Mzi()
                     mzi.layer_num = x_shape - elem_id - 1 # Even side
                     mzi.index = y_shape - diag_id - 1 # Don't divide by 2 to allow half-step offset between layers
                     mzi.delta = delta
                     mzi.sigma = sigma
-                    phase_diff = phase_diff
+                    mzi.phase_diff = phase_diff
+                    mzi.active = True
                     self.insert_mzi(mzi)
-
 
 
     def insert_mzi(self, mzi: Mzi):
         layer_num = mzi.layer_num
         index = mzi.index
+        self.mzi[layer_num][index] = mzi
 
         if layer_num > 0: # Connect predecessors
             if index > 0:
-                if self.mzi[layer_num-1][index-1] != None:
+                if self.mzi[layer_num-1][index-1].active:
                     mzi.upper_pred = self.mzi[layer_num-1][index-1]
                     self.mzi[layer_num-1][index-1].lower_succ = mzi
-                elif self.phase_shifter[layer_num-1][index-1] != None:
+                elif self.phase_shifter[layer_num-1][index-1].active:
                     mzi.upper_pred = self.phase_shifter[layer_num-1][index-1]
                     self.phase_shifter[layer_num-1][index-1].succ = mzi
 
-            if index < self.mzi.shape[0] - 1:
-                if self.mzi[layer_num-1][index+1] != None:
+            if index < len(self.mzi) - 1: # index < self.mzi.shape[0] - 1
+                if self.mzi[layer_num-1][index+1].active:
                     mzi.lower_pred = self.mzi[layer_num-1][index+1]
                     self.mzi[layer_num-1][index+1].upper_succ = mzi
-                elif self.phase_shifter[layer_num-1][index+1] != None:
+                elif self.phase_shifter[layer_num-1][index+1].active:
                     mzi.upper_pred = self.phase_shifter[layer_num-1][index+1]
                     self.phase_shifter[layer_num-1][index+1].succ = mzi
 
-        if layer_num < self.mzi.shape[1] - 1: # Connect successors
+        if layer_num < len(self.mzi[0]) - 1: # Connect successors (all rows are the same length so we check the first)
             if index > 0:
-                if self.mzi[layer_num+1][index-1] != None:
+                # print(f"DEBUG: layer_num = {layer_num}")
+                # print(f"DEBUG: len(self.mzi[0] - 1 = {len(self.mzi[0]) -  1}")
+                if self.mzi[layer_num+1][index-1].active:
                     mzi.upper_pred = self.mzi[layer_num+1][index-1]
                     self.mzi[layer_num+1][index-1].lower_succ = mzi
-                elif self.phase_shifter[layer_num+1][index-1] != None:
+                elif self.phase_shifter[layer_num+1][index-1].active:
                     mzi.upper_pred = self.phase_shifter[layer_num+1][index-1]
                     self.phase_shifter[layer_num+1][index-1].succ = mzi
 
-            if index < self.mzi.shape[0] - 1:
-                if self.mzi[layer_num+1][index+1] != None:
+            if index < len(self.mzi) - 1:
+                if self.mzi[layer_num+1][index+1].active:
                     mzi.lower_pred = self.mzi[layer_num+1][index+1]
                     self.mzi[layer_num+1][index+1].upper_succ = mzi
-                elif self.phase_shifter[layer_num+1][index+1] != None:
+                elif self.phase_shifter[layer_num+1][index+1].active:
                     mzi.upper_pred = self.phase_shifter[layer_num+1][index+1]
                     self.phase_shifter[layer_num+1][index+1].succ = mzi
 
@@ -144,38 +179,7 @@ U = np.array( # CNOT
 compiler = Compiler()
 compiler.compile(U)
 
-
-
-# x = 0
-# y = 0
-#
-# x_shape = U.shape[1]
-# y_shape = U.shape[0]
-#
-# V = deepcopy(U)
-#
-# for diag_id in range(x_shape): # Diagonals starting from the bottom left
-#     for elem_id in range(diag_id + 1):
-#         y = y_shape - diag_id + elem_id - 1
-#         x = elem_id
-#         print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({x}, {y})")
-#
-#         if diag_id % 2 == 0: # j = 1, 3, 5, ... (j starts at 1 while diag_id starts at 0)
-#             # VM
-#             delta = np.atan(V[x][y] / V[x+1][y])
-#             sigma = sympy.angle(V[x][y])
-#             phase_diff = V[x][y] - V[x_shape-1 - x][y_shape-1 - y]
-#
-#             m = np.array([[np.exp(sigma*1j) * np.sin(delta), np.exp(sigma*1j) * np.cos(delta)],\
-#                           [np.exp(sigma*1j) * np.cos(delta), -np.exp(sigma*1j) * np.sin(delta)]])
-#
-#             x_offset = x
-#             y_offset = x_offset
-#
-#             M = np.identity(x_shape)
-#             M[x_offset:x_offset+m.shape[0], y_offset:y_offset+m.shape[1]] = m
-#
-#             V = V @ M
-#
-#         else: # j = 0, 2, 4, 
-#             # MV
+for row in compiler.mzi:
+    for mzi in row:
+        if not mzi.active: continue
+        print(f"mzi({mzi.layer_num}, {mzi.index}): delta = {mzi.delta}, sigma = {mzi.sigma}")
