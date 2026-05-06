1fecf05b80267a58f93bd989e7a453931c0aa1a1
FTNK-LocalAdm <FTNK-LocalAdm@win.dtu.dk>
2026-05-06 09:41:25 +0200

    Characterization issue testing

--- characterization/PhaseShifterCaracterization.py
+++ characterization/PhaseShifterCaracterization.py
@@ -5,74 +5,55 @@ Save the data is the associated folder inside the chip folder.
 #%%######### Import packages ############ 
 from Functions import *
 
-#%% Testing structure
-test_chip = Chip(qontrol="nope", folder=r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session0")
-print(f"Type of chip: {type(test_chip)}")
-print(hasattr(test_chip, 'mzi_list'))
-test_ps1 = PhaseShifter(test_chip, name='')
-test_ps1.in_port  = 4
-test_ps1.out_port = 3
-test_ps1.channel  = 6
-
-test_ps1.chip_config = {
-    'I1': 1
-}
-
-
-test_ps2 = PhaseShifter(test_chip, name='')
-test_ps2.in_port  = 4
-test_ps2.out_port = 3
-test_ps2.channel  = 6
-
-test_ps2.chip_config = {
-    'I2': 2
-}
-
-test_mzi = MZI(chip=test_chip, name="mzi03", shifter1=test_ps1, shifter2=test_ps2, folder=r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session0")
-
-test_chip.add_mzi(test_mzi)
-
-test_chip.save_parameters()
-
-
 ############ 1. Initialize the instruments ############
 #%% List instruments
 list_devices()
+
+
 #%% Qontrol, 4=Receiver, 6=Transmitter, 6=Packaged
-q = Qontrol(COM=6)
+q = Qontrol(COM=4)
 q.reset()
 #%% Powermeter
-wl_pump = 1546.12
-wl_idler = 1552.52
-wl_signal = 1539.77
-wl_qdot_pump = 1528.6
+# wl_pump = 1546.12
+# wl_idler = 1552.52
+# wl_signal = 1539.77
+# wl_qdot_pump = 1528.6
 
-pm = ThorlabsPowermeter(address='USB0::0x1313::0x8078::P0017921::0::INSTR', 
+pm = ThorlabsPowermeter(address='USB0::0x1313::0x8078::P0028334::INSTR', 
                         wavelength=1550)
 
 
 ############# 2. Initialize the chip and the phase shifter ############
 #%% Chip
-chip = Chip(qontrol=q, 
-            folder = r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session0")
-#%% Phase shifter
-ps1 = PhaseShifter(chip, name='')
-ps1.in_port  = 4
-ps1.out_port = 3
-ps1.channel  = 6
 
-ps1.chip_config = {
-    'I1': 1
-}
-ps1.chip_config = {key: value * np.pi for key, value in ps1.chip_config.items()}
-chip.set_config(ps1.chip_config)
+q.reset()
+chip = Chip(qontrol=q, folder=r"C:\Users\FTNK-LocalAdm\QPIC_driver\characterization\data\session0\data_master.json")
+print(f"Type of chip: {type(chip)}")
+print(hasattr(chip, 'mzi_list'))
+ps1 = PhaseShifter(chip, name='H11')
+ps1.in_port  = 10
+ps1.out_port = 10
+ps1.channel  = 11
+ps1.temperature = 8.54
+
+# test_ps1.chip_config = {
+#     'I1': 1
+# }
+
+test_ps2 = PhaseShifter(chip, name='H12')
+
+mzi = MZI(chip=chip, name="MZI0", shifter1=ps1, shifter2=test_ps2, folder=r"C:\Users\FTNK-LocalAdm\QPIC_driver\characterization\data\session0")
+
+chip.add_mzi(mzi)
 
 
 #%%######### 3. Run the electrical characterization ############
 # Parameters of the sweep
 start_v = 0.0 # Starting voltage
 step_v  = 0.1 # Increment voltage
-stop_v  = 6 + step_v # Final voltage
+stop_v  = 12 + step_v # Final voltage
+
+# q.set_v(channel=31, voltage=10)
 
 ps1.run_electrical_sweep(start_v, stop_v, step_v, delay=0.02)
 ps1.run_electrical_fit()
@@ -80,17 +61,74 @@ ps1.plot_electrical_fit()
 ps1.plot_electrical_power()
 
 
+#%%#########
+q.reset()
+for i in range(43):
+    q.set_v(i, 7)
+
+
+#%%#########
+
+q.reset()
+ch2 = 25
+q.set_v(ch2, 7)
+# q.set_maxv(ch2, 12)
+# q.set_maxi(ch2, 50)
+
+print(f"Max voltage: {q.q.vmax[ch2]}")
+print(f"Max current: {q.q.imax[ch2]}")
+
+print(f"Device ID: {type(q.q.device_id)}")
+
+
+#%%#########
+#ps1.channel = 7
+
+start_v = 0.0 # Starting voltage
+step_v  = 0.1 # Increment voltage
+stop_v  = 6 + step_v # Final voltage
+
+# print(f"Max voltage: {q.q.vmax[ch]}")
+# print(f"Max current: {q.q.imax[ch]}")
+
+for ch in range(1, 43):
+    voltage = []
+    current = []
+    for i in np.linspace(start_v, stop_v, 20):
+        #q.set_v(ps1.channel, float(i))
+        q.set_v(ch, float(i))
+        voltage.append(q.read_v(ch))
+        current.append(q.read_i(ch))
+        print(f"voltage: {voltage[-1]}")
+    if max(current) > 3:
+        plt.plot(voltage, current)
+    else:
+        plt.plot(voltage, current, label=str(ch))
+plt.legend()
+plt.show()
+
+
+# q.set_v(ps1.channel, float(6))
+# while(True):
+#     current = q.read_i(ps1.channel)
+#     print(f"commanded voltage: {6}, actual voltage: {q.read_v(ps1.channel)}, current: {current}")
+#     time.sleep(0.5)
+
+
 #%%######### 4. Run the optical characterization ############
 # Parameters of the sweep
+# ps1.channel = 7
+
 start_v = 0.0 # Starting voltage
 step_v  = 0.05 # Increment voltage
-stop_v  = 6 + step_v # Final voltage
+stop_v  = 12 + step_v # Final voltage
 # Optical sweep
 q.set_v(ps1.channel, 0)
 time.sleep(1)
 
 ps1.run_optical_sweep([pm], start_v, stop_v, step_v, delay=0.2)
 print(f'Visibility: {ps1.visibility:.3f}')
+
 #%% Optical power fit
 ps1.run_optical_fit() 
 print(ps1.A, ps1.B, ps1.omega, ps1.phi_0)   
@@ -103,7 +141,7 @@ ps1.plot_optical_fit()
 
 ########## 5. Save data and close instruments ############
 #%% Save data in a json file
-ps1.save_parameters()
+chip.save_parameters()
 #%% Switch off all the heaters
 q.reset()
 
--- characterization/test.py
+++ characterization/test.py
@@ -4,40 +4,40 @@ Save the data is the associated folder inside the chip folder.
 """
 from Functions import *
 
-test_chip = Chip(qontrol="nope", folder=r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session1")
-# test_ps1 = PhaseShifter(test_chip, name='H1')
-# test_ps1.in_port  = 4
-# test_ps1.out_port = 3
-# test_ps1.channel  = 6
+test_chip = Chip(qontrol="nope", folder=r"C:\Users\FTNK-LocalAdm\QPIC_driver\characterization\data\session0\data_master.json")
+test_ps1 = PhaseShifter(test_chip, name='H1')
+test_ps1.in_port  = 4
+test_ps1.out_port = 3
+test_ps1.channel  = 6
 
-# test_ps1.chip_config = {
-#     'I1': 1
-# }
-
-
-# test_ps2 = PhaseShifter(test_chip, name='H2')
-# test_ps2.in_port  = 4
-# test_ps2.out_port = 3
-# test_ps2.channel  = 6
-
-# test_ps2.chip_config = {
-#     'I2': 2
-# }
-
-test_chip.load_params(r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session0/data_master.json")
-
-test_ps3 = PhaseShifter(test_chip, name='H3')
-test_ps3.in_port  = 7
-test_ps3.out_port = 12
-test_ps3.channel  = 8
-
-test_ps4 = PhaseShifter(test_chip, name='H4')
-test_ps4.in_port  = 7
-test_ps4.out_port = 12
-test_ps4.channel  = 8
+test_ps1.chip_config = {
+    'I1': 1
+}
 
 
-test_mzi = MZI(chip=test_chip, name="mzi04", shifter1=test_ps3, shifter2=test_ps4, folder=r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session1")
+test_ps2 = PhaseShifter(test_chip, name='H2')
+test_ps2.in_port  = 4
+test_ps2.out_port = 3
+test_ps2.channel  = 6
+
+test_ps2.chip_config = {
+    'I2': 2
+}
+
+# test_chip.load_params(r"/Users/balage/Documents/@UNI/@KU/@PHASE3/@PROJ/characterization/data/session0/data_master.json")
+
+# test_ps3 = PhaseShifter(test_chip, name='H3')
+# test_ps3.in_port  = 7
+# test_ps3.out_port = 12
+# test_ps3.channel  = 8
+
+# test_ps4 = PhaseShifter(test_chip, name='H4')
+# test_ps4.in_port  = 7
+# test_ps4.out_port = 12
+# test_ps4.channel  = 8
+
+
+test_mzi = MZI(chip=test_chip, name="mzi01", shifter1=test_ps1, shifter2=test_ps2, folder=r"C:\Users\FTNK-LocalAdm\QPIC_driver\characterization/data/session0")
 
 test_chip.add_mzi(test_mzi)
 
