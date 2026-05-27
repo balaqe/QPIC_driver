77bef8ce4558c21953271d35ac94678e579ccd74
Balage <basza22@student.sdu.dk>
2026-05-27 12:02:16 +0200

    Thing works for CNOT??

--- main.py
+++ main.py
@@ -16,16 +16,16 @@ def is_unitary(m):
 #     [0, 0, 0, 0, 0, 1],
 #     [0, 0, 0, 0, 1, 0],
 # ])
-# U = np.array([
-#     [1, 0, 0, 0],
-#     [0, 1, 0, 0],
-#     [0, 0, 0, 1],
-#     [0, 0, 1, 0],
-# ])
+U = np.array([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 1],
+    [0, 0, 1, 0],
+])
 #
 chip_generator = Clements(4)
 # U = unitary_group.rvs(4)
-U = chip_generator.generate_random_unitary()
+# U = chip_generator.generate_random_unitary()
 print(f"The matrix is unitary: {is_unitary(U)}")
 print("INITIAL UNITARY")
 print(np.round(U, 2))
@@ -51,26 +51,40 @@ for row in compiler.mesh_elements:
 chip.build()
 chip.display()
 
-
-sym_state = []
-for i in range(4):
-    sym_state.append(sp.Symbol(f"c{i}"))
-
-state = chip.execute(sym_state)
-
-print("\nResulting state:")
-for i in range(len(state)):
-    for a in sp.preorder_traversal(state[i][0]):
-        if isinstance(a, sp.Float):
-            state[i][0] = state[i][0].subs(a, round(a, 1))
-print(state)
-
-state2 = U @ np.array(sym_state).reshape(-1, 1)
+ex_state = [1, 0, 0, 1]
+print("\nResulting state")
+res = chip.execute(ex_state)
+for i in range(len(ex_state)):
+    res[i] = abs(res[i])**2
+print(np.round(res, 2))
 
 print("\nActual state")
-for i in range(len(state2)):
-    for a in sp.preorder_traversal(state2[i][0]):
-        if isinstance(a, sp.Float):
-            state2[i][0] = state2[i][0].subs(a, round(a, 1))
-print(state2)
+res = U @ np.array(ex_state).reshape(-1, 1)
+for i in range(len(ex_state)):
+    res[i] = abs(res[i])**2
+print(np.round(res, 2))
 
+
+
+# sym_state = []
+# for i in range(4):
+#     sym_state.append(sp.Symbol(f"c{i}"))
+
+# state = chip.execute(sym_state)
+#
+# print("\nResulting state:")
+# for i in range(len(state)):
+#     for a in sp.preorder_traversal(state[i][0]):
+#         if isinstance(a, sp.Float):
+#             state[i][0] = state[i][0].subs(a, round(a, 1))
+# print(state)
+#
+# state2 = U @ np.array(sym_state).reshape(-1, 1)
+#
+# print("\nActual state")
+# for i in range(len(state2)):
+#     for a in sp.preorder_traversal(state2[i][0]):
+#         if isinstance(a, sp.Float):
+#             state2[i][0] = state2[i][0].subs(a, round(a, 1))
+# print(state2)
+#
