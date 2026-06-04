b2b9a67e480d49670413232d8cf450459b6daf85
Balage <basza22@student.sdu.dk>
2026-06-04 10:09:26 +0200

    Diagonalization works for 6x6 as well

--- decomp/tests/arbitrary_q.py
+++ decomp/tests/arbitrary_q.py
@@ -105,17 +105,18 @@ class Compiler:
         for diag_id in range(x_shape-1): # Diagonals starting from the bottom left
             for elem_id in range(diag_id + 1):
             # for elem_id in range(diag_id):
-                print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({y}, {x})")
-                print(f"V = {np.round(V, 2)}")
 
                 # y = y_shape - diag_id + elem_id - 1
                 # x = elem_id
 
                 if diag_id % 2 == 0: # j = 1, 3, 5, ... (j starts at 1 while diag_id starts at 0)
+                    # VM
+                    #
                     x = diag_id - elem_id
                     y = y_shape-1 - elem_id
                     print(f"VM")
-                    # VM
+                    print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({y}, {x})")
+                    print(f"V = {np.round(V, 2)}")
 
                     # sigma = np.angle(V[y][x])
                     sigma = 0
@@ -135,15 +136,9 @@ class Compiler:
                     P[x][x] = np.exp(1j*phase_diff)
                     V = V @ P
 
-                    # print("Before:")
-                    # print(np.round(V, 2))
-                    # V[x][y] *= np.exp(1j*phase_diff, dtype=np.complex128)
-                    # print("After:")
-                    # print(np.round(V, 2))
-
                     delta = 0
                     if V[y][x] != 0:
-                        delta = np.atan(-V[y][x+1] / V[y][x])
+                        delta = np.atan(-V[y][x+1] / V[y][x], dtype=np.complex128)
                     else:
                         delta = np.pi/2 # Pi/2 shift which produces identity
 
@@ -156,6 +151,7 @@ class Compiler:
                     print(f"m = {m}")
 
                     x_offset = x if x < x_shape-2 else x_shape-2
+                    # x_offset = y if y < x_shape-2 else x_shape-2
                     y_offset = x_offset
 
                     M = np.identity(x_shape, dtype=np.complex128)
@@ -174,11 +170,15 @@ class Compiler:
                     self.insert_mzi(mzi)
 
                 else: # j = 0, 2, 4, ...
+                    # MV
+                    print(f"MV")
+
                     y = y_shape - diag_id + elem_id - 1
                     x = elem_id
 
-                    print(f"MV")
-                    # MV
+                    print(f"diag_id = {diag_id}; elem_id = {elem_id}; element{diag_id + elem_id}({y}, {x})")
+                    print(f"V = {np.round(V, 2)}")
+
 
                     # sigma = np.angle(V[y][x])
                     sigma = 0
@@ -200,7 +200,7 @@ class Compiler:
 
                     delta = 0
                     if V[y][x] != 0:
-                        delta = np.atan(V[y-1][x] / V[y][x])
+                        delta = np.atan(V[y-1][x] / V[y][x], dtype=np.complex128)
                     else:
                         delta = np.pi/2
                     print(f"delta = {delta}")
@@ -211,7 +211,8 @@ class Compiler:
 
                     # x_offset = x if x < x_shape-2 else x_shape-2
                     # x_offset = x + 1 if x + 1 < x_shape-2 else x_shape-3
-                    x_offset = x+1
+                    # x_offset = x+1
+                    x_offset = x_shape-diag_id-2 +x
                     y_offset = x_offset
 
                     M = np.identity(x_shape, dtype=np.complex128)
